fac70213852c11888ac85a993a670c32581592f7
gregmcknight <gmcknight23@gmail.com>
2022-01-12 17:50:50 -0500

    tasks 1-4 and 6 done

--- index.js
+++ index.js
@@ -123,11 +123,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, number) {
+  return `${array[number].name} gave the restaurant a ${array[number].rating} star review, and their feedback was: ${array[number].feedback}`
 }
 
-
+console.log('task 7: ', getReviewByIndex(reviews, 0));
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
