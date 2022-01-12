f95efb43407e2fe90f6e415dbcde279359645864
gregmcknight <gmcknight23@gmail.com>
2022-01-12 16:29:18 -0500

    1 b done

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ function createMenuItem(name, price, category){
 }
 
 
-console.log('task 1 a', createMenuItem('tacos', 8, 'Lunch'))
+console.log('task 1 a', createMenuItem('tacos', 8, 'Lunch'));
+
+
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
 Test your createMenuItems function by doing the following:
@@ -30,8 +32,9 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
-
+console.log('task 1 b', createMenuItem('pizza', 5, 'lunch'));
+console.log('task 1 b', createMenuItem('pancakes', 7, 'breakfast'));
+console.log('task 1 b', createMenuItem('pbj', 3, 'snack'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
